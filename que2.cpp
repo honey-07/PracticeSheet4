67b8ff7b941a40417fb87384de8adf732b00d5a8
HONEY PATEL <123378526+honey-07@users.noreply.github.com>
2023-09-10 14:19:46 +0530

    Update que2.cpp

--- que2.cpp
+++ que2.cpp
@@ -1,98 +1,92 @@
-/* Write a C++ Program to perform following-
-• compare two strings
-• concate two strings
-• copy the string
-• find length of string
-• Count the number of words and characters in a
-sentence.
-*/
-#include <iostream>
-#include <string>
+// Write a C++ program (using function overloaded) to sort 5 integer values, or 5 long values, or 5 double values.
+#include<iostream>
 using namespace std;
 
+class overload
+{
+public:
+	void sort(int n[5])
+		{
+			int temp;
+		for (int i = 0; i < 5; i++)
+		{
+			for (int j = i+1; j < 5; ++j)
+			{
+				if (n[i] > n[j])
+				{
+					temp=n[i];
+					n[i]=n[j];
+					n[j]=temp;
+				}
+			}
+			
+		}
+		cout << "integer sorting : " << endl;
+		for (int i = 0; i < 5; ++i)
+		{
+			
+			cout << n[i] << endl;
+		}
+
+	}
+	void sort(long n[5])
+	{
+		long temp;
+		for (int i = 0; i < 5; i++)
+		{
+			for (int j = i+1; j < 5; ++j)
+			{
+				if (n[i] > n[j])
+				{
+					temp=n[i];
+					n[i]=n[j];
+					n[j]=temp;
+				}
+			}
+		}
+		cout << "long sorting : " << endl;
+		for (int i = 0; i < 5; ++i)
+		{
+			
+			cout << n[i] << endl;
+		}
+
+		
+	}
+	void sort(double n[5])
+	{
+		double temp;
+		for (int i = 0; i < 5; i++)
+		{
+			for (int j = i+1; j < 5; ++j)
+			{
+				if (n[i] > n[j])
+				{
+					temp=n[i];
+					n[i]=n[j];
+					n[j]=temp;
+				}
+			}
+		}
+
+		cout << "double sorting : " << endl;
+		for (int i = 0; i < 5; ++i)
+		{
+			
+			cout << n[i] << endl;
+
+		}
+	}
+};
 int main()
 {
-	int choice;
+	overload a;
+	int arr1[5] = {5,3,7,1,4};
+	long arr2[5] = {54,38,79,31,4};
+	double arr3[5] = {5.6,3.4,7.0,1.6,4.9};
 
-	cout << "Enter your choice : ";
-	cin >> choice;
-
-	cin.ignore();
-	switch(choice)
-	{
-		case 1:
-			{
-				string str,str2;
-				cout << "Enter string 1 : ";
-				cin >> str;
-				cout << "Enter string 2 : ";
-				cin >> str2;
-
-				if(str==str2)
-					cout<< "BOTH THE STRINGS ARE EQUAL" << endl;
-				else
-					cout<< "BOTH THE STRINGS ARE NOT EQUAL" << endl;
-			}
-			break;
-		case 2:
-			{
-				string str,str2;
-				cout << "Enter string 1 : ";
-				cin >> str;
-				cout << "Enter string 2 : ";
-				cin >> str2;
-
-				str.append(str2);
-				cout << "STRING IS : " << str;
-			}
-			break;	
-		case 3:
-			{
-				string str;
-				cout << "Enter string : ";
-				getline(cin, str);
-				char str2[15];
-				str.copy(str2,5,3);
-				cout << "STRING IS : "<< str2;
-				
-			}	
-			break;
-		case 4:
-			{
-				string str;
-				cout << "Enter string : ";
-				getline(cin, str);
-				cout << "LENGTH OF STRING IS : "<< str.length();
-			}	
-			break;
-		case 5:
-			{
-				string sentence;
-				int word=0,character=0;
-
-				cout << "Enter string : ";
-				getline(cin, sentence);
-				
-				for(char c : sentence)
-				{
-					if(c==' ')
-					{
-						word++;
-					}
-					else
-					{
-						character++;
-					}
-				}
-				word++;
-				cout << "TOTAL WORDS : " << word << endl;
-				cout << "TOTAL CHARACTERS : " << character << endl;
-			}
-			break;
-
-		default:
-			cout << "INVALID CHOICE.";
-			break;
-	}
-	return 0;
+	a.sort(arr1);
+	a.sort(arr2);
+	a.sort(arr3);
+return 0;
 }
